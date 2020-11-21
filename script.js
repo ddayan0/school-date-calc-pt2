2880d3946da4c59d0b1cefc5c88635b868ef11d8
Dennis D <denday198@gmail.com>
2020-11-21 00:33:24 +0000

    fixed modulus with parseInt()

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // dennis dayan
 function datecalc(){
-  var day = new Date(document.getElementById("datepicker").value );
+  var day = new Date(document.getElementById("datepicker").value + " 00:00");
   var dayStr = day.toDateString();
   var firstDay =  new Date("09/8/2020");
   var numDays = (day.getTime()-firstDay.getTime())/(1000 * 60 * 60 * 24)
@@ -8,41 +8,42 @@ function datecalc(){
   var presDay = new Date("02/15/2021");
   var memDay = new Date("05/31/21");
   if (mlkDay.getTime()>day.getTime()){
-      if (numDays % 2 == 0){
+      if (parseInt(numDays % 2) == 0){
         var result =  day.toDateString()+": A day."
       }
       else{
-        var result = day.toDateString()+":  B day."
+		  console.log(numDays % 2);
+		var result = day.toDateString()+":  B day."
       }
   }
-  if (mlkDay.getTime()<day.getTime() && presDay.getTime()>day.getTime()){
-    if (numDays % 2 == 1){
+  else if (mlkDay.getTime()<day.getTime() && presDay.getTime()>day.getTime()){
+    if (parseInt(numDays % 2) == 1){
       var result =  day.toDateString()+": A day."
     }
     else{
       var result = day.toDateString()+":  B day."
     }
   }
-  if (presDay.getTime()<day.getTime() && memDay.getTime()>day.getTime()){
-    if (numDays % 2 == 0){
+  else if (presDay.getTime()<day.getTime() && memDay.getTime()>day.getTime()){
+    if (parseInt(numDays % 2) == 0){
         var result =  day.toDateString()+": A day."
     }
     else{
         var result = day.toDateString()+":  B day."
     }
   }
-  if (memDay.getTime()<day.getTime()){
-    if (numDays % 2 == 1){
+  else if (memDay.getTime()<day.getTime()){
+    if (parseInt(numDays % 2) == 1){
       var result =  day.toDateString()+": A day."
     }
     else{
       var result = day.toDateString()+":  B day."
     }
   }
-  if (dayStr.includes("Jun 22")){
+  else if (dayStr.includes("Jun 22")){
     var result = "Graduation Day/Last Day of School: A day."
   }
-  if(result == null){
+  else if(result == null){
     var result = "Invalid Date"
   }
   document.getElementById("result").innerHTML = (result)
